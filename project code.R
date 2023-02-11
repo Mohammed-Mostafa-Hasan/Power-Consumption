b55b8e13e29f330baa5e59378fed1ec232a1ea26
mohammed mostafa <m.mostafa.eng.zu@gmail.com>
2023-02-11 21:38:31 +0200

    remove spaces

--- project code.R
+++ project code.R
@@ -8,15 +8,10 @@ head(power_city_data)
 #exclude city1 and city2 power consumption for the first model 
 power_city_data <- power_city_data[-c(8,9)]
 #convert character into datetime object
-
 power_city_data$DateTime <- as.POSIXct(power_city_data$DateTime, format = "%m/%d/%Y %H:%M")
 
 
 power_city_data$time <- format(power_city_data$DateTime, "%H:%M:%S")
-
-
-
-
 
 match <- regexpr(".....",power_city_data$time)
 power_city_data$time<-regmatches(power_city_data$time,match)
